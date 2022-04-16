f22e6bdb52d8e9d82674ea168cc46768e92dc3c1
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-04-16 08:14:38 +0530

    basic info added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import data from './data.json';
 
 function App() {
     return (
         <>
             <h1>Samridh</h1>
-            <h2>react!!!</h2>
+            <p>{data.intro}</p>
+            <h2>I am a:</h2>
+            <ul>
+                {data.roles.map((role, index) => (
+                    <li key={index}>{role}</li>
+                ))}
+            </ul>
+            <h2>Projects</h2>
+            {data.projects.map((project, index) => (
+                <section key={index} id={project.name}>
+                    <h3>
+                        {project.directLink ? (
+                            <h3>
+                                <a href={project.directLink}>{project.name}</a>
+                            </h3>
+                        ) : (
+                            <h3>{project.name}</h3>
+                        )}
+                    </h3>
+                    <b>
+                        <a href={project.githubLink}>Github Repo</a>
+                    </b>
+                    <p>{project.about}</p>
+                </section>
+            ))}
+            <h2>Programming Skills:</h2>
+            <ul>
+                {data.programmingSkills.map((skill, index) => (
+                    <li key={index}>{skill}</li>
+                ))}
+            </ul>
+            <h2>General Tech Skills:</h2>
+            <ul>
+                {data.techSkills.map((skill, index) => (
+                    <li key={index}>{skill}</li>
+                ))}
+            </ul>
         </>
     );
 }
