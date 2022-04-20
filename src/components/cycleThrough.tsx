3e6850cd4167cff84c8859cb14bd2b9b86dee2f2
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-04-20 21:58:46 +0530

    content changed; image added

--- src/components/cycleThrough.tsx
+++ src/components/cycleThrough.tsx
@@ -6,6 +6,14 @@ type arrayForCycle = { array: string[]; delay?: number };
 export default function CycleTrough({ array, delay = 1000 }: arrayForCycle) {
     const [currentIndex, setCurrentIndex] = useState<number>(0);
 
+    let maxLen = 0;
+    array.forEach(el => {
+        if (el.length > maxLen) maxLen = el.length;
+    });
+    const paddedArray = array.map(
+        el => el + '\u00A0'.repeat(maxLen - el.length)
+    );
+
     useEffect(() => {
         const interval = setInterval(
             () => setCurrentIndex((currentIndex + 1) % array.length),
@@ -14,6 +22,6 @@ export default function CycleTrough({ array, delay = 1000 }: arrayForCycle) {
         return () => clearInterval(interval); // cleanup function (equivalent to componentDidUnmount)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentIndex]); // run whenever the currentIndex is changed
-
-    return <b>{array[currentIndex]}</b>;
+    // console.log(paddedArray[currentIndex]);
+    return <b>{paddedArray[currentIndex]}</b>;
 }
--- src/pages.tsx
+++ src/pages.tsx
@@ -4,6 +4,7 @@ import Imp from './components/imp';
 import OutboundLink from './components/outboundLink';
 import { general, projects } from './data';
 import { GithubIcon, ExplorerIcon } from './icons';
+import mainPicture from './Coffee_Structure.jpeg';
 
 type page = {
     title: string;
@@ -18,7 +19,10 @@ export const pages: page[] = [
         content: (
             <>
                 <h1>Hello 👋, I am Samridh!</h1>
-                <span style={{ fontSize: '2rem' }}>
+                <div className='main-picture-container'>
+                    <img className='main-picture' src={mainPicture} />
+                </div>
+                <span id='cycling-roles'>
                     I am a:{' '}
                     <Imp>
                         <CycleTrough array={general.roles} />
@@ -28,7 +32,7 @@ export const pages: page[] = [
         ),
     },
     {
-        title: 'About',
+        title: 'About Me',
         link: '/about',
         content: (
             <>
@@ -39,14 +43,14 @@ export const pages: page[] = [
                     <Imp>BTech in Computer Science and Engineering</Imp>.{' '}
                     {general.intro}
                 </p>
-                <h2>Things I am good at</h2>
-                <h3>Programming:</h3>
+                <h1>Things I am good at</h1>
+                <h2>Programming:</h2>
                 <ul>
                     {general.programmingSkills.map((skill, index) => (
                         <li key={index}>{skill}</li>
                     ))}
                 </ul>
-                <h3>General Tech</h3>
+                <h2>General Tech</h2>
                 <ul>
                     {general.techSkills.map((skill, index) => (
                         <li key={index}>{skill}</li>
@@ -56,32 +60,63 @@ export const pages: page[] = [
         ),
     },
     {
-        title: 'Education',
+        title: 'Experience\n&&\nEducation',
         link: '/education',
         content: (
             <>
-                <h1>Education</h1>
-                <h2>Btech in Computer Science and Education</h2>
-                <b>September, 2020 to Present</b>
-                <p>
-                    I am currently a 2nd Year UG student at{' '}
-                    <Imp>VIT, Chennai</Imp>. I have an 8.84 GPA accross 3
-                    semesters.
-                </p>
-                <h2>
-                    Class XII<sup>th</sup> (PCM)
-                </h2>
-                <b>June, 2020</b>
-                <p>
-                    I completed my class 12<sup>th</sup> education from{' '}
-                    <Imp>Millennium School, Lucknow</Imp>. I studied science and
-                    scored 91.2%.
-                </p>
+                <section id='experience'>
+                    <h1>Experience</h1>
+                    <h2>
+                        Winning Team Member of VIT Cyscom's Cyber Defenders CTF
+                        Event
+                    </h2>
+                    <b>Held on 20th April, 2022</b>
+                    <p>
+                        Cyber Defenders was a capture-the-flag event conducted
+                        by Cyscom, VIT Chennai's chapter of OWASP. It consisted
+                        of finding flags hidded in plain sight by using our
+                        cybersecurity and hacking knowledge. My team stood first
+                        in the event.
+                    </p>
+                    <h2>Member of Linux Club</h2>
+                    <b>October 2021 to Present</b>
+                    <p>
+                        A club dedicated to promoting Linux and Free and Open
+                        Source Software (FOSS). I work on their{' '}
+                        <OutboundLink to='https://lugvitc.github.io'>
+                            website
+                        </OutboundLink>{' '}
+                        and helped organise the{' '}
+                        <OutboundLink to='https://lugvitc.github.io/#/events#adrenaline'>
+                            Adrenaline/FOSS Day
+                        </OutboundLink>{' '}
+                        event.
+                    </p>
+                </section>
+                <section id='education'>
+                    <h1>Education</h1>
+                    <h2>B.Tech in Computer Science and Education</h2>
+                    <b>September, 2020 to Present</b>
+                    <p>
+                        I am currently a 2nd Year UG student at{' '}
+                        <Imp>VIT, Chennai</Imp>. I have an 8.84 GPA accross 3
+                        semesters.
+                    </p>
+                    <h2>
+                        Class XII<sup>th</sup> (PCM)
+                    </h2>
+                    <b>June, 2020</b>
+                    <p>
+                        I completed my class 12<sup>th</sup> education from{' '}
+                        <Imp>Millennium School, Lucknow</Imp>. I studied science
+                        and scored 91.2%.
+                    </p>
+                </section>
             </>
         ),
     },
     {
-        title: 'Projects',
+        title: 'My\nProjects',
         link: '/projects',
         content: (
             <>
@@ -113,47 +148,41 @@ export const pages: page[] = [
         ),
     },
     {
-        title: 'Experience',
-        link: '/experience',
+        title: 'Contact Me',
+        link: '/contact',
         content: (
             <>
-                <h1>Experience</h1>
-                <h2>
-                    Winning Team Member of VIT Cyscom's Cyber Defenders CTF
-                    Event
-                </h2>
-                <b>Held on 20th April, 2022</b>
+                <h1>Contact Me!</h1>
+                <p>Wanna ask something, collaborate or just have a talk?</p>
                 <p>
-                    Cyber Defenders was a capture-the-flag event conducted by
-                    Cyscom, VIT Chennai's chapter of OWASP. It consisted of
-                    finding flags hidded in plain sight by using our
-                    cybersecurity and hacking knowledge. My team stood first in
-                    the event.
+                    Contact me through, LinkedIn, Instagram DM's or just shoot
+                    me an email (my id is samridh.anand.paatni at gmail).
                 </p>
-                <h2>Member of Linux Club</h2>
-                <b>October 2021 to Present</b>
+                <h1>Credits</h1>
                 <p>
-                    A club dedicated to promoting Linux and Free and Open Source
-                    Software (FOSS). I work on their{' '}
-                    <OutboundLink to='https://lugvitc.github.io'>
-                        website
+                    This website was made using{' '}
+                    <OutboundLink to='https://reactjs.org/'>
+                        React.js
                     </OutboundLink>{' '}
-                    and helped organise the{' '}
-                    <OutboundLink to='https://lugvitc.github.io/#/events#adrenaline'>
-                        Adrenaline/FOSS Day
+                    and{' '}
+                    <OutboundLink to='https://www.typescriptlang.org/'>
+                        typescript
                     </OutboundLink>{' '}
-                    event.
+                    hosted using Github Pages.
+                    <br />
+                    All of the icons are from{' '}
+                    <OutboundLink to='https://simpleicons.org/'>
+                        simple icons
+                    </OutboundLink>
+                    .
+                </p>
+                <h1>Copyleft</h1>
+                <p>
+                    This website (and thus the github repo) and the coffee
+                    molecule structure image (on the home page) are open-source
+                    under the GNU GPL v-2 license.
                 </p>
             </>
         ),
     },
-    // {
-    //     title: 'Contact',
-    //     link: '/contact',
-    //     content: (
-    //         <>
-    //             <h1>Contact Me!</h1>
-    //         </>
-    //     ),
-    // },
 ];
